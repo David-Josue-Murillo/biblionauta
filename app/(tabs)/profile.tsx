b687791d24fc94641eeaac1d9f1d9d0697496706
David Murillo <david.josue.murillo23@gmail.com>
2025-07-06 19:06:12 -0500

    Integrando los componentes en la pantalla de perfil

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,9 +1,31 @@
-import { Text, View } from "react-native";
+import { View, ScrollView } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { COLORS } from "../../src/constants/colors";
+import { userData, menuItems } from "../../src/mocks/profileData";
+import { UserHeader } from "../../src/components/Profile/UserHeader";
+import { MenuSection } from "../../src/components/Profile/MenuSection";
+import { LogoutButton } from "../../src/components/Profile/LogoutButton";
 
 export default function ProfileScreen() {
   return (
-    <View>
-      <Text>Pantalla para la sección de configuración y perfil del usuario</Text>
+    <View
+      className="flex-1"
+      style={{ backgroundColor: COLORS.background.primary }}
+    >
+      <StatusBar style="light" />
+
+      <ScrollView className="flex-1" showsVerticalScrollIndicator={false}>
+        <UserHeader userData={userData} />
+
+        {/* Menú de opciones */}
+        <View className="mx-4 mb-8">
+          {menuItems.map((section) => (
+            <MenuSection key={section.id} section={section} />
+          ))}
+        </View>
+
+        <LogoutButton />
+      </ScrollView>
     </View>
-  )
+  );
 }
