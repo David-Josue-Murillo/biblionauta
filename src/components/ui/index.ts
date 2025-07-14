857e745a7925bb7de9283e2cb4ea81afe49b16a6
David Murillo <david.josue.murillo23@gmail.com>
2025-07-14 18:03:53 -0500

    feat(ui): Agregue los componentes HeaderBar y SectionCard para mejorar los elementos de la interfaz de usuario.

---
+++ src/components/ui/index.ts
@@ -0,0 +1,4 @@
+export { HeaderBar, type HeaderBarProps } from './HeaderBar'
+export { SectionCard, type SectionCardProps } from './SectionCard'
+export { SwitchRow, type SwitchRowProps } from './SwitchRow'
+export { InfoCard, type InfoCardProps } from './InfoCard' 
