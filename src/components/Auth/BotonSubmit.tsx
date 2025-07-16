d9ceba9abc7aba4799c4e6aac749a21770fafca1
David Murillo <david.josue.murillo23@gmail.com>
2025-07-16 14:56:08 -0500

    refactor(boton-submit): Agregar interfaz TypeScript para propiedades en el componente BotonSubmit

--- src/components/Auth/BotonSubmit.tsx
+++ src/components/Auth/BotonSubmit.tsx
@@ -1,7 +1,12 @@
-import { TouchableOpacity, Text } from 'react-native'
+import { Text, TouchableOpacity } from 'react-native'
 import { colors } from '../../constants/theme'
 
-export default function BotonSubmit({ action, text }) {
+interface BotonSubmitProps {
+  action: () => void
+  text: string
+}
+
+export default function BotonSubmit({ action, text }: BotonSubmitProps) {
   return (
     <TouchableOpacity onPress={action}>
       <Text className="text-sm font-bold" style={{ color: colors.primary }}>
