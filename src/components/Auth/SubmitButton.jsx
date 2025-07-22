3d97f81f141bf86fd9c7aa4009ae1e2f98b6536b
David Murillo <david.josue.murillo23@gmail.com>
2025-07-21 22:45:38 -0500

    refactor: Reordenar las declaraciones de importación en el componente SubmitButton para mantener la coherencia

--- src/components/Auth/SubmitButton.jsx
+++ src/components/Auth/SubmitButton.jsx
@@ -1,4 +1,4 @@
-import { TouchableOpacity, Text } from 'react-native'
+import { Text, TouchableOpacity } from 'react-native'
 import { colors } from '../../constants/theme'
 
 const SubmitButton = ({
